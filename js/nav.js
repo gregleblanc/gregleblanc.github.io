f5c20657b47b68a68ae52c2bbaa714c8bfb1277d
gregleblanc <gregleblanc227@gmail.com>
2014-11-18 09:37:51 -0500

    deleted img fix + nav mobile

--- js/nav.js
+++ js/nav.js
@@ -4,14 +4,14 @@
 
 	document.querySelector('.nav-btn').addEventListener('click', function(e){
 	e.preventDefault();
-	
+
 	if (navTop.getAttribute('data-state') == 'expanded'){
-		
+
 		navTop.setAttribute('data-state', 'collapsed');
 	}else{
-		
+
 		navTop.setAttribute('data-state', 'expanded');
-		
+
 	}
-	
+
 });
